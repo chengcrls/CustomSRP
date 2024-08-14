67a114d1cea8426a2d61aee177877a89145aa9df
WangChangze <hugh.wang@unity.cn>
2024-08-14 18:32:42 +0800

    1

--- Assets/CustomRP/Runtime/CameraRender.cs
+++ Assets/CustomRP/Runtime/CameraRender.cs
@@ -3,10 +3,14 @@ using System.Collections.Generic;
 using UnityEditor.VersionControl;
 using UnityEngine;
 using UnityEngine.Rendering;
-public class CameraRender
+public partial class CameraRender
 {
     ScriptableRenderContext context;
+    CullingResults cullingResults;
+    //这个和Always是不一样的，它们有什么差别呢？
+    static ShaderTagId unlitShaderTagId = new ShaderTagId("SRPDefaultUnlit");
 
+    static Material errorMaterial;
     Camera camera;
 
     const string bufferName = "Render Camera";
@@ -22,14 +26,31 @@ public class CameraRender
         this.context = context;
         this.camera = camera;
 
+        if (!Cull())
+        {
+            return;
+        }
+
         Setup();
         DrawVisibleGeometry();
+        DrawUnsupportedShaders();
         Submit();
     }
 
     void DrawVisibleGeometry()
     {
+        var sorttingSettings = new SortingSettings(camera)
+        {
+            criteria = SortingCriteria.CommonOpaque
+        };
+        var drawingSetting = new DrawingSettings(unlitShaderTagId,sorttingSettings);
+        var filteringSettings = new FilteringSettings(RenderQueueRange.opaque);
+        context.DrawRenderers(cullingResults, ref drawingSetting, ref filteringSettings);
         context.DrawSkybox(camera);
+        sorttingSettings.criteria = SortingCriteria.CommonTransparent;
+        drawingSetting.sortingSettings = sorttingSettings;
+        filteringSettings.renderQueueRange = RenderQueueRange.transparent;
+        context.DrawRenderers(cullingResults,ref drawingSetting,ref filteringSettings);
     }
     void Submit()
     {
@@ -50,5 +71,15 @@ public class CameraRender
     {
         context.ExecuteCommandBuffer(buffer);
         buffer.Clear();
+    }
+
+    bool Cull()
+    {
+        if (camera.TryGetCullingParameters(out ScriptableCullingParameters p))
+        {
+            cullingResults=context.Cull(ref p);
+            return true;
+        }
+        return false;
     }
 }
